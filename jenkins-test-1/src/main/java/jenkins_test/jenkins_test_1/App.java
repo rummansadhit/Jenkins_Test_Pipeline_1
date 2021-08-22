07039f5df7c81f5305dd8f5e3751450f53616b81
rummansadhit <rummansadhit@gmail.com>
2021-08-22 17:58:47 -0400

    initial Commit

---
+++ jenkins-test-1/src/main/java/jenkins_test/jenkins_test_1/App.java
@@ -0,0 +1,13 @@
+package jenkins_test.jenkins_test_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.print("Hello World");
+    }
+}
